aad2fa5766bdb68d8571cf12b8eb8fd076adbd1e
Aldashov Bekzhan <aldashov.bekzhan@gmail.com>
2023-12-04 21:46:42 +0600

    saving filter paramters in URL

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import qs from "qs";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import { setFilters } from "./redux/slices/filterSlice";
 
 import "./scss/app.scss";
 
 function App() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const isSearch = React.useRef(false);
+  const isMounted = React.useRef(false);
+
   const activeCategoryInd = useSelector(
     (state) => state.filter.activeCategoryInd
   );
@@ -18,7 +26,8 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoadingPizzas, setIsLoadingPizzas] = React.useState(true);
 
-  React.useEffect(() => {
+  function fetchPizzas() {
+    setIsLoadingPizzas(true);
     fetch(
       `https://656852f29927836bd9748b19.mockapi.io/items?category=${
         activeCategoryInd > 0 ? activeCategoryInd : ""
@@ -29,6 +38,43 @@ function App() {
         setPizzas(arr);
         setIsLoadingPizzas(false);
       });
+  }
+
+  React.useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+
+      dispatch(setFilters(params));
+
+      isSearch.current = true;
+
+      console.log(window.location.search);
+      console.log("set filters useEffect, no variables");
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (!isSearch.current) {
+      fetchPizzas();
+      console.log("fetching pizzas useEffect, category and sort indexes");
+    }
+    isSearch.current = false;
+  }, [activeCategoryInd, sortBy]);
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortBy,
+        activeCategoryInd,
+      });
+
+      navigate(`?${queryString}`);
+
+      console.log(
+        "changing url parameters useEffect, category and sort indexes"
+      );
+    }
+    isMounted.current = true;
   }, [activeCategoryInd, sortBy]);
 
   return (
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -12,19 +12,24 @@ export const filterSlice = createSlice({
   name: "filter",
   initialState,
   reducers: {
-    setActiveCategoryInd: (state, action) => {
+    setActiveCategoryInd(state, action) {
       state.activeCategoryInd = action.payload;
     },
-    setActiveSortInd: (state, action) => {
+    setActiveSortInd(state, action) {
       const value = action.payload;
       state.sort.activeSortInd = value;
       if (value === 0) state.sort.sortBy = "rating";
       else if (value === 1) state.sort.sortBy = "price";
       else state.sort.sortBy = "name";
     },
+    setFilters(state, action) {
+      state.activeCategoryInd = Number(action.payload.activeCategoryInd);
+      state.sort.sortBy = action.payload.sortBy;
+    },
   },
 });
 
-export const { setActiveCategoryInd, setActiveSortInd } = filterSlice.actions;
+export const { setActiveCategoryInd, setActiveSortInd, setFilters } =
+  filterSlice.actions;
 
 export default filterSlice.reducer;
